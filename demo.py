85b2133d5c2c26242651b85ed8c74e5bf1c5a6f5
Akash <krishnanag1996@gmail.com>
2020-05-04 19:49:47 +0530

    section 1- 5

--- demo.py
+++ demo.py
@@ -1,2 +1,31 @@
 import  selenium
 print('test')
+
+a= [10, 20, 30, 40, 50]
+l = ('a', 10, 'alas', 1.3)
+
+print(l)
+
+
+a.append((19))
+
+print(a)
+
+a.insert(3, 'akash')
+print(a)
+
+
+new = {
+    'first': 'akash',
+    'second':'Krishnan'
+}
+
+
+
+print(new)
+print(new['first'])
+
+
+new['common'] = 'testing automation'
+
+print(new)
